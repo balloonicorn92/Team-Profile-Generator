708d705296417f21c0591703d81f2ba25716d877
Fernanda Frers <fernanda.frers@gmail.com>
2021-04-24 16:23:53 -0500

    template literal and push complete

--- index.js
+++ index.js
@@ -1,3 +1,166 @@
 const fs = require('fs')
+const path = require('path')
 const inquirer = require('inquirer')
-const Employee = require('./lib/Employee')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const render = require('./src/template')
+const outputDir = path.resolve(__dirname, 'dist')
+const outputFile = path.join(outputDir, 'index.html')
+
+const teamMembersArr = []
+
+const runApp = () => {
+    console.log('Welcome! Please begin by choosing your first employee')
+    const promptStuff = () => {
+    inquirer
+    .prompt([{
+        type: 'list',
+        name: 'role',
+        message:'What is the role of your employee?',
+        choices:['Manager', 'Engineer', 'Intern', 'My team is complete!']
+    }
+    ])
+    .then(response => {
+        switch (response.role){
+            case 'Engineer': 
+                addEngineer();
+            break
+            case 'Intern':
+                addIntern()
+            break
+            case 'Manager':
+                addManager()
+            break
+
+            default: 
+            renderTeam()
+        }
+    })
+}
+    addEngineer = () => {
+        inquirer
+        .prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'What is the first name of your Engineer?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your engineers ID number?',
+            validate: dataInput => {
+                if (isNaN(dataInput)) { 
+                    console.log('\nIDs cannot contain letters or characters')
+                    return false
+                } else {
+                    return true
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your engineers e-mail address?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your engineers GitHub account?'
+        }])
+        .then(response => {
+            const engineer = new Engineer(response.name, response.id, response.email, response.github)
+
+            teamMembersArr.push(engineer)
+            promptStuff()
+        })
+    }
+    addManager = () => {
+        inquirer
+        .prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your Manager?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your managers ID number?',
+            validate: dataInput => {
+                if (isNaN(dataInput)) { 
+                    console.log('\nIDs cannot contain letters or characters')
+                    return false
+                } else {
+                    return true
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your managers e-mail address?'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your managers office number?',
+            validate: dataInput => {
+                if (isNaN(dataInput)) { 
+                    console.log('\nIDs cannot contain letters or characters')
+                    return false
+                } else {
+                    return true
+                }
+            }
+        }])
+        .then(response => {
+            const manager = new Manager (response.name, response.id, response.email, response.officeNumber)
+
+            teamMembersArr.push(manager)
+            promptStuff()
+        })
+    }
+    addIntern = () => {
+        inquirer
+        .prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your intern?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your interns ID number?',
+            validate: dataInput => {
+                if (isNaN(dataInput)) { 
+                    console.log('\nIDs cannot contain letters or characters')
+                    return false
+                } else {
+                    return true
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your interns e-mail address?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is your interns school name?'
+        }])
+        .then(response => {
+            const intern = new Intern(response.name, response.id, response.email, response.school)
+
+            teamMembersArr.push(intern)
+            promptStuff()
+        })
+    }
+    const renderTeam = () => {
+        fs.writeFileSync(outputFile, render(teamMembersArr), 'UTF-8')
+    }
+    promptStuff()
+}
+  
+runApp()
--- src/template.js
+++ src/template.js
@@ -1,20 +1,113 @@
-const generateEmployee = employee => {
-    const generateIntern = engineer => {
-        return `
-        <section>
-        <h1>${engineer.getName()}<h1>
-        <div>lorem sinetgubg ior fghdssfj othsjdhf bfsdhfgdsioryehsbvndsm</div>
-        <ul>
-        <li>${engineer.getId()}</li>
-        <li>${engineer.getEmail()}</li>
-        </ul>
-        </section>`
-    }
+const generateEngineer = engineer => {
+    return `
+    <div class="col">
+    <div class="card shadow">
+      <div class="card-body bg-primary text-white">
+        <h4 class="card-title">${engineer.getName().charAt(0).toUpperCase() + engineer.getName().slice(1)}</h5>
+        <h5 class="card-text"><i class="fas fa-glasses"></i> ${engineer.getRole()}</h6>
+      </div>
+      <ul class="list-group bg-light" style="padding: 10px">
+        <li class="list-group-item">ID: </br>${engineer.getId()}</li>
+        <li class="list-group-item">E-mail: </br><a href="mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+        <li class="list-group-item">GitHub: </br><a href="github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+      </ul>
+    </div>
+  </div>`
+}
+const generateIntern = intern => {
+    return `
+    <div class="col">
+    <div class="card shadow">
+      <div class="card-body bg-primary text-white">
+        <h4 class="card-title">${intern.getName().charAt(0).toUpperCase() + intern.getName().slice(1)}</h5>
+        <h5 class="card-text"><i class="fas fa-user-graduate"></i> ${intern.getRole()}</h6>
+      </div>
+      <ul class="list-group bg-light" style="padding: 10px">
+        <li class="list-group-item">ID: </br>${intern.getId()}</li>
+        <li class="list-group-item">E-mail: </br><a href="mailto: ${intern.getEmail()}">${intern.getEmail()}</a></li>
+        <li class="list-group-item">School: </br>${intern.getSchool()}</li>
+      </ul>
+    </div>
+  </div>`
+}
+const generateManager = manager => {
+    return `
+    <div class="col">
+    <div class="card shadow">
+      <div class="card-body bg-primary text-white">
+        <h4 class="card-title">${manager.getName().charAt(0).toUpperCase() + manager.getName().slice(1)}</h5>
+        <h5 class="card-text"><i class="fas fa-mug-hot"></i> ${manager.getRole()}</h6>
+      </div>
+      <ul class="list-group bg-light" style="padding: 10px">
+        <li class="list-group-item">ID: </br>${manager.getId()}</li>
+        <li class="list-group-item">E-mail: </br><a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+        <li class="list-group-item">Office Number: </br>${manager.getOffice()}</li>
+      </ul>
+    </div>
+  </div>`
+}
+const generateEmployee = team => {
+    const html = [];
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+    return html.join("");
+}
+// export function to generate entire page
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 jumbotron mb-3 team-heading bg-danger text-white">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+                ${generateEmployee(team)}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `;
 }
 
-// for loop for each one of these employee 
-//for () { if employee[i].getType() === "employee") {
-    //generateEngineer(employee[i])} else if { employee[i].getType() === "Intern" {
-        //generateIntern(employee[i])
-    //}}
-//}}
+        // // for loop for each one of these employee 
+        // for (let i = 0; i < html.length; i++) {
+        //     const employee = html[i]
+        //  if (employee.getType() === "engineer") {
+        //     return generateEngineer(employee)
+        // } else if (employee.getType() === "Intern"){
+        //     return generateIntern(employee)
+        // }else if (employee.getType() === 'manager'){
+        //     return generateManager(employee)
+        // }
+        // }
